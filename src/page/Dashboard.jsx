b6e403a87b5c06ab46ea74d2279ebf50e831bd6f
Saikiran-mobius <rathod.s@mobiusdtaas.ai>
2025-05-17 15:52:41 +0530

    Added the Perfomance simulation card

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -4,6 +4,8 @@ import SideNav from '../component/Dashboard/SideNav'
 import TopNav from '../component/Dashboard/TopNav'
 import GlobalContainer from '../component/Global/GlobalContainer'
 import { useGetAllProducts } from '../API/Query/query'
+import Performance  from '../component/Dashboard/Performance'
+
 
 function Dashboard() {
 
@@ -18,15 +20,16 @@ function Dashboard() {
             <div className='flex flex-col w-full h-full '>
                 <TopNav />
 
-                <div className='p-5 flex flex-col gap-5 h-full'>
+                <div className='p-5 flex flex-col gap-5 h-full overflow-y-auto'>
                     <div className='bg-gray-100 h-96 '>
                         Sainssh
                     </div>
 
-                    <div className=' flex  justify-between items-center w-full h-full gap-5'>
-                        <div className='w-full bg-blue-300 h-full'>
+                    <div className=' flex  justify-between items-center w-full h-auto gap-5 '>
+                        <div className='w-full bg-white h-full rounded-md'>
 
-                            <div class=" p-4 bg-accent-green">Card Background</div>
+                            <Performance/>
+                            
 
 
                         </div>
