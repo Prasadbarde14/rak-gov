808bbf1520b7d0537573cf5b8cc981c08faead5c
Saikiran-mobius <rathod.s@mobiusdtaas.ai>
2025-05-19 11:35:37 +0530

    added query for interact api

--- src/components/AIAssistantWidget/AIAssistantWidget.jsx
+++ src/components/AIAssistantWidget/AIAssistantWidget.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import {useChatBotMutation } from '../../API/Query/query'
 
 export default function AIAssistantWidget() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +16,13 @@ export default function AIAssistantWidget() {
   const chartData = {}; // Replace with actual chart data if needed
   const [expandedMessages, setExpandedMessages] = useState({});
   const [uuid, setUuid] = useState(Math.random().toString(36).substring(2, 15));
+
+    const { mutateAsync, isPending, isError } = useChatBotMutation();
+
+
+
+
+
   const toggleExpanded = (index) => {
     setExpandedMessages((prev) => ({
       ...prev,
@@ -24,30 +32,20 @@ export default function AIAssistantWidget() {
 
   const chatBotResponse = async (query, agent = "prompt") => {
     setLoading(true);
-    const headers = {
-      token:
-        "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3Ny1NUVdFRTNHZE5adGlsWU5IYmpsa2dVSkpaWUJWVmN1UmFZdHl5ejFjIn0.eyJleHAiOjE3MjYxODIzMzEsImlhdCI6MTcyNjE0NjMzMSwianRpIjoiOGVlZTU1MDctNGVlOC00NjE1LTg3OWUtNTVkMjViMjQ2MGFmIiwiaXNzIjoiaHR0cDovL2tleWNsb2FrLmtleWNsb2FrLnN2Yy5jbHVzdGVyLmxvY2FsOjgwODAvcmVhbG1zL21hc3RlciIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJmNzFmMzU5My1hNjdhLTQwYmMtYTExYS05YTQ0NjY4YjQxMGQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJIT0xBQ1JBQ1kiLCJzZXNzaW9uX3N0YXRlIjoiYmI1ZjJkMzktYTQ3ZC00MjI0LWFjZGMtZTdmNzQwNDc2OTgwIiwibmFtZSI6ImtzYW14cCBrc2FteHAiLCJnaXZlbl9uYW1lIjoia3NhbXhwIiwiZmFtaWx5X25hbWUiOiJrc2FteHAiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJwYXNzd29yZF90ZW5hbnRfa3NhbXhwQG1vYml1c2R0YWFzLmFpIiwiZW1haWwiOiJwYXNzd29yZF90ZW5hbnRfa3NhbXhwQG1vYml1c2R0YWFzLmFpIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiLyoiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtbWFzdGVyIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7IkhPTEFDUkFDWSI6eyJyb2xlcyI6WyJIT0xBQ1JBQ1lfVVNFUiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwic2lkIjoiYmI1ZjJkMzktYTQ3ZC00MjI0LWFjZGMtZTdmNzQwNDc2OTgwIiwidGVuYW50SWQiOiJmNzFmMzU5My1hNjdhLTQwYmMtYTExYS05YTQ0NjY4YjQxMGQiLCJyZXF1ZXN0ZXJUeXBlIjoiVEVOQU5UIn0=.FXeDyHBhlG9L4_NCeSyHEaNEBVmhFpfSBqlcbhHaPaoydhKcA0BfuyHgxg_32kQk6z5S9IQ7nVKS2ybtOvwo0WyLWwLQchSq7Noa7LooHIMzmeWMQb_bLKtbaOti59zwIdS8CkfGaXut7RUQKISQVWmbUGsVJQa2JkG6Ng_QN0y5hFVksMWPZiXVsofQkJXHXV1CQ3gabhhHKo3BqlJwzpsCKLDfg1-4PmSl1Wqbw03Ef2yolroj5i8FoeHukOQPkwCUHrrNw-ilIp917nqZa89YbCMtDjWyaj8pEH7GJR5vMZPE2WcJPn5dSA1IHVunfatEB1cDAitaFjVNWNnddQ",
-
-      "Content-Type": "application/json",
-    };
 
     try {
-      const response = await axios.post(
-        "https://ig.gov-cloud.ai/mobius-gpt-service/v2/interact",
-        {
-          agentId: "03dba2df-93b0-4c28-b57f-7d15c69a3abf",
-          query: `${agent} - ${query} ${
-            agent === "data" ? JSON.stringify(chartData) : ""
-          }`,
-          sessionId: uuid,
-        },
-        { headers }
-      );
+      const botResponse = await mutateAsync({
+        query: input,
+        agent: "prompt",
+        chartData,
+        uuid,
+      });
+      console.log("data: ",botResponse);
+      // const message = await getChatBotResponse();
 
-      const message = response.data.agentReasoning[0].messages[0];
       const botMessage = {
         sender: "bot",
-        text: message,
+        text: botResponse,
         timestamp: new Date().toISOString(),
       };
       setMessages((prev) => [...prev, botMessage]);
