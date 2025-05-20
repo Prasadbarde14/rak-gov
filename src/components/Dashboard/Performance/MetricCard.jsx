47ef0b481db440e9a6f8553b5018e38319571f78
Sainssh-Mobius <sharma.s@mobiusdtaas.ai>
2025-05-20 10:32:09 +0530

    Running simmulations

--- src/components/Dashboard/Performance/MetricCard.jsx
+++ src/components/Dashboard/Performance/MetricCard.jsx
@@ -5,7 +5,7 @@ import MaintenanceOverview from "./MaintenanceOverview";
 import MaintenanceMetric from "./MaintenanceMetrics";
 
 
-const MetricCard = ({ data }) => {
+const MetricCard = ({ data,index,selected }) => {
   const {
     title,
     current,
@@ -26,19 +26,20 @@ const MetricCard = ({ data }) => {
   const handleFieldChange = (value) => {
     setSelectedFields((prev) =>
       prev.includes(value)
-        ? prev.filter((item) => item !== value) // Deselect
-        : [...prev, value] // Select and preserve order
+        ? prev.filter((item) => item !== value)
+        : [...prev, value] 
     );
   };
 
   const renderSelectedComponent = (field) => {
+
     switch (field) {
       case "project":
-        return <ProjectPlanningOverview key="project" />;
+        return <ProjectPlanningOverview key="project"  index={index} selected={selected} parentData={data}/>;
       case "maintenanceOverview":
-        return <MaintenanceOverview key="maintenanceOverview" />;
+        return <MaintenanceOverview key="maintenanceOverview" index={index} selected={selected} parentData={data}/>;
       case "maintenanceMetric":
-        return <MaintenanceMetric key="maintenanceMetric" />;
+        return <MaintenanceMetric key="maintenanceMetric"  index={index} selected={selected} parentData={data}/>;
       default:
         return null;
     }
