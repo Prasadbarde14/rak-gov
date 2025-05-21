7ffa677ffdd252e74117e5f86d14752642eb8443
Anshu Kumar <kumar.a@mobiusdtaas.ai>
2025-05-21 10:04:39 +0530

    fixed redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import {Provider} from 'redux';
 import { store } from './Redux/Store/store.js'
+import { Provider } from 'react-redux'
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
