39d21c4db9b4eb0979fe1aee74da6a6aa3cf9cfe
Saikiran-mobius <rathod.s@mobiusdtaas.ai>
2025-05-23 12:38:11 +0530

    added rak 404 page

--- src/page/404/NotFound.jsx
+++ src/page/404/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import rakLogo from '../../../public/rak-logo.png';
+import rakLogo from '../../assets/rak-logo.png';
 
 const NotFound = () => {
   return (
